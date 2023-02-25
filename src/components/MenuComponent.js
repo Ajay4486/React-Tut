6dde2f0fabb3e36d93786ce0d355bb6a5bbc8085
Ajay Shitkar <ajshitkar282@gmail.com>
2023-02-25 17:46:59 +0530

    Functional Component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
 
-class Menu extends Component {    //This is important when u define any class component
+    //This is important when u define any class component
    
-   constructor(props) {
-      super(props);                  // stores properties related to component 
-      
-     
-   }
+
  
-   render() {                                           //return corresponding view for this component
-      const menu = this.props.dishes.map((dish) => {
+                                             //return corresponding view 
+                                             
+                                             //for this component
+
+      function RenderMenuItem({ dish, onClick}) {
+         return(
+            <Card onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+               <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+         </Card>
+
+         );
+      }  
+      
+      const Menu = (props)  => {
+         const menu = props.dishes.map((dish) => {
+            return (
+               <div key={dish.id} className="col-12 col-md-5 m-1">
+                 <RenderMenuItem dish = {dish} onClick={props.onClick}/>
+               </div>
+            );
+         });
+   
          return (
-            <div key={dish.id} className="col-12 col-md-5 m-1">
-               <Card onClick={() => this.props.onClick(dish.id)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                     <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-               </Card>
+            <div className="container">
+               <div className="row">
+                  {menu}
+               </div>
             </div>
          );
-      });
-
-      return (
-         <div className="container">
-            <div className="row">
-               {menu}
-            </div>
-         </div>
-      );
-   }
-}
-
+         
+      }
+      
 export default Menu;            //necessary whenever u want this in other file
